ffb8713bea3b762ba463210c528830800b39ae39
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2026-01-22 01:38:58 +0500

    spline

--- assignment-4/spline.py
+++ assignment-4/spline.py
@@ -5,12 +5,8 @@ import numpy as np
 def spline(x: list[float], y: list[float], dx0: float, dxn: float) -> list[Callable[[float], float]]:
 	n = len(x) - 1
 
-	h: list[float] = []
-	b: list[float] = []
-
-	for i in range(n):
-		h.append(x[i + 1] - x[i])
-		b.append((y[i + 1] - y[i]) / h[i])
+	h: list[float] = [x[i + 1] - x[i] for i in range(n)]
+	b: list[float] = [(y[i + 1] - y[i]) / h[i] for i in range(n)]
 
 	v: list[float] = [0] * (n + 1)
 	u: list[float] = [0] * (n + 1)
@@ -24,33 +20,38 @@ def spline(x: list[float], y: list[float], dx0: float, dxn: float) -> list[Calla
 	for i in range(1, n):
 		u[i] = 2 * (h[i - 1] + h[i])
 		v[i] = h[i]
-		w[i] = h[-1]
+		w[i] = h[i - 1]
 		q[i] = 6 * (b[i] - b[i - 1])
 
 	u[n] = 2 * h[n - 1]
 	w[n] = h[n - 1]
 	q[n] = 6 * (dxn - b[n - 1])
 
-	T: list[list[float]] = []
+	T: list[list[float]] = [[0] * (n + 1) for _ in range(n + 1)]
 
 	for i in range(n + 1):
-		if i == 0:
-			T.append([u[i], v[i], 0])
-		elif i == n:
-			T.append([0, w[i], u[n]])
-		else:
-			T.append([w[i], u[i], v[i]])
+		T[i][i] = u[i]
+		if i < n:
+			T[i][i + 1] = v[i]
+
+		if i > 0:
+			T[i][i - 1] = w[i]
 
 	z: list[float] = np.linalg.solve(np.array(T), np.array(q)).tolist()
 
 	S: list[Callable[[float], float]] = []
 
 	for i in range(n):
+		xi, xi1 = x[i], x[i + 1]
+		yi, yi1 = y[i], y[i + 1]
+		hi = h[i]
+		zi, zi1 = z[i], z[i + 1]
+
 		S.append(lambda x_star: (\
-			z[i + 1] / (6 * h[i])) * (x_star - x[i]) ** 3\
-			+ (z[i] / (6 * h[i])) * (x[i + 1] - x_star) ** 3\
-			+ (y[i + 1] / h[i] - ((h[i] * z[i + 1]) / 6)) * (x_star - x[i])\
-			+ (y[i] / h[i] - (h[i] * z[i]) / 6) * (x[i + 1] - x_star))
+			zi1 / (6 * hi)) * (x_star - xi) ** 3\
+			+ (zi / (6 * hi)) * (xi1 - x_star) ** 3\
+			+ (yi1 / hi - ((hi * zi1) / 6)) * (x_star - xi)\
+			+ (yi / hi - (hi * zi) / 6) * (xi1 - x_star))
 
 	return S
 
@@ -60,4 +61,4 @@ if __name__ == "__main__":
 
 	s = spline(x, y, 0, 0)
 
-	print(s[0](1))
+	print(s[1](4))
