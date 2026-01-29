7ecd4cfd1c14b745662c55683d1dae6fceb5da58
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2026-01-29 15:04:31 +0500

    taylor: use scipy derivative instead of approximation

--- assignment-6/taylor.py
+++ assignment-6/taylor.py
@@ -1,20 +1,14 @@
 import math
 from collections.abc import Callable
 
-def dfdx(f: Callable[[float, float], float], x: float, y: float, eps: float = 0.000001) -> float:
-	return (f(x + eps, y) - f(x - eps, y)) / (2 * eps)
-
-def dfdy(f: Callable[[float, float], float], x: float, y: float, eps: float = 0.000001) -> float:
-	return (f(x, y + eps) - f(x, y - eps)) / (2 * eps)
+from scipy import differentiate
 
 def taylor(f: Callable[[float, float], float], x0: float, y0: float, xt: float, n: int) -> float:
-	derivatives = [0] * (n + 1)
-
-	derivatives[1] = f(x0, y0)
+	derivatives: list[float] = [0, f(x0, y0)] + [0] * n
 
 	for k in range(2, n + 1):
-		fx = dfdx(f, x0, y0)
-		fy = dfdy(f, y0, x0)
+		fx: float = differentiate.derivative(lambda x: f(x, y0), x0, order=1).df
+		fy: float = differentiate.derivative(lambda y: f(x0, y), y0, order=1).df
 
 		derivatives[k] = fx + fy * derivatives[k - 1]
 
